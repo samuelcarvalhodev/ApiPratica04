0306750791b06632d3be1e223ef36c50873a3f74
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2021-07-28 01:22:57 -0300

    Rotas Carro

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import express, { Router } from 'express';
-import path from 'path';
-import UserController from '../controllers/UserController';
+import CarroController from '../controllers/CarroController';
 
 
 const routes = Router();
 
-routes.post('/user/', UserController.add);
-routes.get('/user/', UserController.getAll);
+routes.post('/carro/', CarroController.add);
+routes.get('/carro/', CarroController.getAllActive);
+routes.get('/carro/quantidade/:qtd', CarroController.getByQtd);
+routes.get('/carro/:id', CarroController.getById);
+routes.get('/carro/brand/:brand', CarroController.getByBrand);
+routes.put('/carro/:id', CarroController.update);
+routes.delete('/carro/:id', CarroController.delete);
 
 export default routes;
